42e9b23459f07a67a019d23a16f8126390ca557f
Law Horne <foxtrottwist@ymail.com>
2018-12-15 21:14:13 -0500

    Production build configuration

--- mix.exs
+++ mix.exs
@@ -42,7 +42,8 @@ defmodule Vmeste.MixProject do
       {:phoenix_live_reload, "~> 1.2-rc", only: :dev},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:distillery, "~> 2.0"}
     ]
   end
 
